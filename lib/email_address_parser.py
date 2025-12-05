d15faf9c33559ce882776ac1ae32382b5e9538d2
danielkamweru <daniel.kamweru@student.moringaschool.com>
2025-12-05 14:43:21 +0300

    completed lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,10 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, string):
+        self.string = string
+
+    def parse(self):
+        email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        emails = re.findall(email_regex, self.string)
+        return sorted(emails)
